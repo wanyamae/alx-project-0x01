a63d1b58e49998ddf64c894f3b834f11bd08a3a8
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-07-19 21:09:53 +0300

    Remove unused components and update global styles for improved theming and layout

--- alx-project-0x01/components/layout/Footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer: React.FC = () => {
-    return (
-        <footer className="bg-gray-800 text-white p-4 text-center">
-            <p className="text-sm">© 2023 Your Company. All rights reserved.</p>
-        </footer>
-    )
-}
-export default Footer;
--- alx-project-0x01/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header: React.FC = () => {
-    return (
-        <header className="bg-gray-800 text-white p-4">
-            <h1 className="text-2xl font-bold">Header Component</h1>
-        </header>
-    )
-}
-
-export default Header;
